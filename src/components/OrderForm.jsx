b0350d4ba55e8609161bbaa8ff17f7d8e2ce4e40
PabloSaba <psabaliauskas@teco.com.ar>
2019-11-05 22:38:16 -0300

    thanks Lint, I love you

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -57,7 +57,7 @@ const useStyles = makeStyles(theme => ({
                  setMenu(res.data);
                   })
 
-     }, []);
+     }, [params.match.params.number]);
      const menuInit = {
 
          "name": "Milanesas",
