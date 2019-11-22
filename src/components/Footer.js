19027665e278f2f19a78ca2f4a7d677d31a8e596
sirDemian <sirdemian@gmail.com>
2019-11-22 14:36:13 -0300

    fix style picture profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React from "react";
-import logo from "../assets/cactus.png"
+import logo from "../assets/12Cactus.png"
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="" />
       <img src={logo} style={{height :'80px'}} alt={'12Cactus'}/>
     <p>
-      Powered by <a href="https://unquipedia.com.ar">Unquipedia</a>
+      Powered by #12Cactus
+    </p>
+    <p>
+      Sponsored by <a href="https://unquipedia.com.ar">Unquipedia</a>
     </p>
   </footer>
 );
